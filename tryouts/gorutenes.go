2c1db6edccbdc335230caa8e3f8b9a49a419ecff
abo3adel <abo3adel35@gmail.com>
2025-07-15 00:08:23 +0300

    feat:try: goroutines select

--- tryouts/gorutenes.go
+++ tryouts/gorutenes.go
@@ -16,17 +16,29 @@ func Sum(nums []int, chnl chan int) {
 }
 
 func Gorotenses() {
-	nums1 := []int{5, 7, 9, 10, 15, 25}
+	nums1 := []int{5, 7, 9, 10, 25}
 	nums2 := []int{3, 4, 8, 6, 9, 12}
+	nums3 := []int{5, 7, 10, 2, 8, 25}
 
 	chnl := make(chan int, len(nums1))
+	chnl3 := make(chan int, len(nums3))
 
 	go Sum(nums1, chnl)
 	go Sum(nums2, chnl)
+	go Sum(nums3, chnl3)
 
-	x, y := <-chnl, <-chnl
+	// x, y := <-chnl, <-chnl
 
-	fmt.Println(x)
-	fmt.Println(y)
+	select {
+		case y := <- chnl:
+			fmt.Println("y1: ", y)
+		case x := <- chnl3:
+			fmt.Println("x3: ", x)
+		case <- time.After(5 * time.Second):
+			fmt.Println("ennnnnnd")
+	}
+
+	// fmt.Println(x)
+	// fmt.Println(y)
 
 }
